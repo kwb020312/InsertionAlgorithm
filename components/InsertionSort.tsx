4705c66313a0ac13ab044bd38d049e34f55ba3e4
kimwoobin <kwb020312@naver.com>
2020-10-10 14:32:27 +0900

    Delayed For Sorting My Graph

--- components/InsertionSort.tsx
+++ components/InsertionSort.tsx
@@ -1,22 +1,68 @@
 import { range, shuffle } from "lodash";
-import { useState } from "react";
+import { Dispatch, FC, SetStateAction, useState } from "react";
 
-const SIZE = 10;
+const SIZE = 20;
 const getArr = () => shuffle(range(1, SIZE));
 
-const sort = (arr: number[]) => {
+const delaySetArr = (
+  arr: number[],
+  setArr: Dispatch<SetStateAction<number[]>>
+) => {
+  return new Promise((resolve) => {
+    setArr([...arr]);
+    setTimeout(resolve, 10);
+  });
+};
+
+const sort = async (
+  arr: number[],
+  setArr: Dispatch<SetStateAction<number[]>>
+) => {
   for (let i = 1; i < arr.length; i++) {
     let key = arr[i];
     let j = i - 1;
 
     while (j >= 0 && arr[j] > key) {
       arr[j + 1] = arr[j];
+      await delaySetArr(arr, setArr);
       j = j - 1;
     }
 
     arr[j + 1] = key;
   }
   return arr;
+};
+
+interface PropsBar {
+  value: number;
+  i: number;
+}
+
+const Bar: FC<PropsBar> = ({ value, i }) => {
+  return (
+    <>
+      <div
+        key={i}
+        style={{
+          height: value * 50,
+          display: "inline-block",
+          transform: `translateX(${i * 21})`,
+          fontSize: 5,
+          marginRight: 1,
+        }}
+        className="bar"
+      >
+        {value}
+      </div>
+      <style jsx>{`
+        .bar {
+          width: 20px;
+          background-color: black;
+          margin-top: 15px;
+        }
+      `}</style>
+    </>
+  );
 };
 
 export default function InsertionSort() {
@@ -26,28 +72,16 @@ export default function InsertionSort() {
     setArr(getArr());
   };
 
-  const handleSort = () => {
+  const handleSort = async () => {
     const newArr = [...arr];
-    setArr(sort(newArr));
+    setArr(await sort(newArr, setArr));
   };
 
   return (
     <div>
       <div className="board">
         {arr.map((value, i) => (
-          <div
-            key={i}
-            style={{
-              height: value * 100,
-              display: "inline-block",
-              transform: `translateX(${i * 21})`,
-              fontSize: 5,
-              marginRight: 1,
-            }}
-            className="bar"
-          >
-            {value}
-          </div>
+          <Bar key={i} value={value} i={i} />
         ))}
       </div>
       <div className="buttonBox">
@@ -70,11 +104,6 @@ export default function InsertionSort() {
           height: 60px;
           background-color: gray;
           text-align: right;
-        }
-        .bar {
-          width: 20px;
-          background-color: black;
-          margin-top: 15px;
         }
       `}</style>
     </div>

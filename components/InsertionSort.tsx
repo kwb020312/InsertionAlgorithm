c960343fb3924c6bebd4f43d24a366349a31a3ed
kimwoobin <kwb020312@naver.com>
2020-10-10 14:49:07 +0900

    Add IndexI , IndexJ For Users

--- components/InsertionSort.tsx
+++ components/InsertionSort.tsx
@@ -16,20 +16,27 @@ const delaySetArr = (
 
 const sort = async (
   arr: number[],
-  setArr: Dispatch<SetStateAction<number[]>>
+  setArr: Dispatch<SetStateAction<number[]>>,
+  setIdxI: TSetIdx,
+  setIdxJ: TSetIdx,
+  setIsRunning: TSetIsRunning
 ) => {
   for (let i = 1; i < arr.length; i++) {
+    setIdxI(i);
     let key = arr[i];
     let j = i - 1;
+    setIdxJ(j);
 
     while (j >= 0 && arr[j] > key) {
       arr[j + 1] = arr[j];
       await delaySetArr(arr, setArr);
       j = j - 1;
+      setIdxJ(j);
     }
 
     arr[j + 1] = key;
   }
+  setIsRunning(false);
   return arr;
 };
 
@@ -37,6 +44,8 @@ interface PropsBar {
   value: number;
   i: number;
 }
+type TSetIdx = Dispatch<SetStateAction<number>>;
+type TSetIsRunning = Dispatch<SetStateAction<boolean>>;
 
 const Bar: FC<PropsBar> = ({ value, i }) => {
   return (
@@ -67,6 +76,9 @@ const Bar: FC<PropsBar> = ({ value, i }) => {
 
 export default function InsertionSort() {
   const [arr, setArr] = useState(getArr());
+  const [idxI, setIdxI] = useState(1);
+  const [idxJ, setIdxJ] = useState(1);
+  const [isRunning, setIsRunning] = useState(false);
 
   const handleShuffle = () => {
     setArr(getArr());
@@ -74,7 +86,8 @@ export default function InsertionSort() {
 
   const handleSort = async () => {
     const newArr = [...arr];
-    setArr(await sort(newArr, setArr));
+    setIsRunning(true);
+    setArr(await sort(newArr, setArr, setIdxI, setIdxJ, setIsRunning));
   };
 
   return (
@@ -84,9 +97,28 @@ export default function InsertionSort() {
           <Bar key={i} value={value} i={i} />
         ))}
       </div>
+      <div
+        className="index i"
+        style={{ transform: `translateX(${idxI * 20}px)` }}
+      >
+        i
+      </div>
+      <div
+        className="index j"
+        style={{ transform: `translateX(${idxJ * 20}px)` }}
+      >
+        j
+      </div>
       <div className="buttonBox">
-        <button onClick={handleShuffle}>shuffle</button>
-        <button onClick={handleSort}>sort</button>
+        {isRunning === false ? (
+          <>
+            {" "}
+            <button onClick={handleShuffle}>shuffle</button>
+            <button onClick={handleSort}>sort</button>
+          </>
+        ) : (
+          <h1>Now Running...</h1>
+        )}
       </div>
       <style jsx>{`
         .board {
@@ -104,6 +136,20 @@ export default function InsertionSort() {
           height: 60px;
           background-color: gray;
           text-align: right;
+        }
+        .index {
+          position: absolute;
+          width: 20px;
+
+          opacity: 0.5;
+        }
+        .index.i {
+          background-color: blue;
+          color: white;
+        }
+        .index.j {
+          background-color: yellow;
+          color: white;
         }
       `}</style>
     </div>
